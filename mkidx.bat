9a9cb513d7013dbc3490ddf96d6ca65237a5f814
Gahan Saraiya <gahan.saraiya@intel.com>
2019-12-15 14:39:06 +0530

    updated clean up batch

--- mkidx.bat
+++ mkidx.bat
@@ -1,1 +1,11 @@
-if exist main.gls (del main.gls && makeindex main.glo -s main.ist -o main.gls)
+del *.acn
+del *.aux
+del *.glo
+del *.ilg
+del *.ist
+del *.lof
+del *.log
+del *.out
+del *.toc
+
+del main.gls && makeindex main.glo -s main.ist -o main.gls
